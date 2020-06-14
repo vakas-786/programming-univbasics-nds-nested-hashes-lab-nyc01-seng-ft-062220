b3ea50ec6e76ed5daf95fff8aa09c10dd642a7fc
Vakas  Akhtar <vakhtar@fordham.edu>
2020-06-14 03:01:43 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -25,5 +25,5 @@ end
 def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
-  assembled_aoh
+  assembled_aoh[1][:name]
 end
