0a3c8640407f62e35c6bb1fb817b4540b4d637f2
Vakas  Akhtar <vakhtar@fordham.edu>
2020-06-14 03:02:17 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -25,6 +25,6 @@ end
 def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
-  assembled_aoh[1][:name]
+  assembled_aoh[1][:name] = "Joelle van Dyne (PGOAT)"
   return assembled_aoh
 end
